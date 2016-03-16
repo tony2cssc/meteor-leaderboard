d84c87d860fde76cd70a2a386093fc0cb7c75b5c
tony2cssc <jijing.guo@gmail.com>
2016-03-17 00:24:50 +0800

    finish template

--- meteor-leaderboard.js
+++ meteor-leaderboard.js
@@ -1,23 +1,10 @@
+playersList = new Mongo.Collection('players');
+console.log("Hello world");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+  console.log("Hello Client");
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+  console.log("Hello Server");
 }
